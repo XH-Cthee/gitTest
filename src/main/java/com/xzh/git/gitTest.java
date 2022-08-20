0a4075348eaf14f26342facce4473c2635373c1b
xzh <coldf0519@163.com>
2022-08-20 17:25:26 +0800

    first commit

---
+++ src/main/java/com/xzh/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.xzh.git;
+
+/**
+ * @Author:Xzh_
+ * @Date:2022/8/20
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
